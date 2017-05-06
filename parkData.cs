221d1c870f1c4f439cc4405559db1155f3551aa7
PavaniJagarlamudi <pavani.jagarlamudi5@gmail.com>
2017-05-05 21:06:30 -0400

    Add files via upload

---
+++ parkData.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApplication.Models
+{
+    public class parkData
+    {
+        public ObjectId Id { get; set; }
+        public String parkSlotId { get; set; }
+        public String isParkingAvailable { get; set; }
+        public String assignedTagId { get; set; }
+
+    }
+}
